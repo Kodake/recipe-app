4bb6d7e9a893fc6a33c29bfcdd9934e6d1c86d43
Kodake <kodake@hotmail.es>
2022-03-29 14:43:11 -0400

    Add delete method for recipe details

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,10 +9,15 @@ import Searchbar from './Searchbar';
 
 const Navbar = () => {
     const { color } = useTheme();
+    const { changeColor } = useTheme();
+
+    const defaultColor = () => {
+        changeColor('#58249c');
+    }
 
     return (
         <div className='navbar' style={{ background: color }}>
-            <nav>
+            <nav onClick={defaultColor}>
                 <Link to={'/'} className='brand'>
                     <h1>Cooking ninjas</h1>
                 </Link>
--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import { useFetch } from '../../hooks/useFetch';
 import { useTheme } from '../../hooks/useTheme';
+
+// Icon Image
+import deleteIcon from '../../assets/delete-icon.svg';
 
 // Styles
 import './Recipe.css';
@@ -12,6 +16,8 @@ interface Props {
 
 const Recipe = () => {
     const params = useParams<Props>();
+    const [recipeId, setRecipeId] = useState('');
+    const { deleteData, recipes } = useFetch(`http://localhost:3000/recipes/${recipeId}`, 'DELETE');
     const url = `http://localhost:3000/recipes/${params.id}`;
     const { recipe, isPending, error } = useFetch(url);
     const history = useHistory();
@@ -25,7 +31,36 @@ const Recipe = () => {
             }, (2000));
         }
 
-    }, [error, history])
+    }, [error, history]);
+
+    const handleDelete = (id: string) => {
+        setRecipeId(id);
+        Swal.fire({
+            icon: 'warning',
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            focusDeny: false,
+            heightAuto: false,
+            focusCancel: false,
+            focusConfirm: false,
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteData(recipeId);
+                // Redirect to HomePage
+                history.push('/');
+                recipes?.filter(recipe => recipe.id !== recipeId);
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        });
+    }
 
     return (
         <div className={`recipe ${mode}`}>
@@ -33,6 +68,11 @@ const Recipe = () => {
             {isPending && <p className='loading'>Loading...</p>}
             {recipe && (
                 <>
+                    <img
+                        className='delete'
+                        src={deleteIcon}
+                        onClick={() => handleDelete(recipe.id)}
+                    />
                     <h2 className='page-title'>{recipe.title}</h2>
                     <p>Takes {recipe.cookingTime} to cook...</p>
                     <ul>
